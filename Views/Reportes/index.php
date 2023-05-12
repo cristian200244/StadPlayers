b29e7d5df3d58ea72735eaa918ea921d6270151d
Mario  Echeverry <alejoecheverry33@gmail.com>
2023-05-11 22:59:10 -0500

    query  select jugadores genReport

--- Views/Reportes/index.php
+++ Views/Reportes/index.php
@@ -1,21 +1,25 @@
 <?php
 
-include_once(__DIR__ . "../../../config/rutas.php");
-?>
-<?php
-include_once(BASE_DIR . "../../Views/main/header.php");
-include_once(BASE_DIR . "../../Views/partials/aside.php");
-?>
+require_once(__DIR__ . "../../../config/rutas.php");
+require_once(BASE_DIR . "../../Views/main/header.php");
+require_once(BASE_DIR . "../../Views/partials/aside.php");
+require_once(BASE_DIR . '../../Models/GenerarReportesModel.php');
 
+//Instancia
+$reportes = new ReportesModel();
+
+//Reporte
+$jugadores = $reportes->getPlayers();
+
+?>
 <div class="imgGenReport">
     <div id="layoutAuthentication">
         <div id="layoutAuthentication_content">
             <main>
-                <div class="container">
+                <div class="container my-3">
                     <div class="row justify-content-center">
                         <div class="col-lg-7">
-                            <div class="card shadow-lg border-0 rounded-lg   justify-content-center"
-                                style="margin-top: 30%;">
+                            <div class="card shadow-lg border-0 rounded-lg   justify-content-center" style="margin-top: 30%;">
                                 <div class="card-header bg-black text-info">
                                     <h3 class="text-center font-weight-light my-4">Generar Reporte</h3>
                                 </div>
@@ -25,14 +29,11 @@ include_once(BASE_DIR . "../../Views/partials/aside.php");
                                             <div class="col-md-6">
                                                 <div class="form-floating mb-3 mb-md-0">
                                                     <h3>Fecha Inicial</h3>
-
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="form-floating">
-
                                                     <input class="form-control" type="date" name="fechaInicial" />
-
                                                 </div>
                                             </div>
                                         </div>
@@ -41,13 +42,11 @@ include_once(BASE_DIR . "../../Views/partials/aside.php");
                                             <div class="col-md-6">
                                                 <div class="form-floating mb-3 mb-md-0">
                                                     <h3>Fecha Final</h3>
-
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="form-floating mb-3 mb-md-0">
-                                                    <input class="form-control" class="form-control" type="date"
-                                                        name="fechaFinal" />
+                                                    <input class="form-control" class="form-control" type="date" name="fechaFinal" />
                                                 </div>
                                             </div>
                                         </div>
@@ -55,29 +54,18 @@ include_once(BASE_DIR . "../../Views/partials/aside.php");
                                             <div class="col-md-6">
                                                 <div class="form-floating mb-3 mb-md-0">
                                                     <h3>Nombre del Jugador</h3>
-
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
-                                                <div class="dropdown">
-                                                    <a class="btn btn-success dropdown-toggle" href="#" role="button"
-                                                        id="dropdownMenuLink" data-bs-toggle="dropdown"
-                                                        aria-expanded="false">
-                                                        Elegir jugador
-                                                    </a>
-
-                                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                                        <li><a class="dropdown-item" href="#">Something else here</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
+                                                <select name="jugadores" id="jugadores">
+                                                    <?php foreach ($jugadores as $jugador) : ?>
+                                                        <option value="<?= $jugador->id ?>"><?= $jugador->getNombreCompleto()  ?></option>";
+                                                    <?php endforeach ?>
+                                                </select>
                                             </div>
                                         </div>
                                         <div class="mt-4 mb-0">
-                                            <div class="d-grid"><a class="btn btn-primary btn-block"
-                                                    href="login.html">Generar</a></div>
+                                            <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Generar</a></div>
                                         </div>
                                     </form>
                                 </div>
