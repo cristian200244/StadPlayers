59ec8057644dbe523ac79e6100ca78f5338d4e20
kevin mendoza <kevin.obed1999@gmail.com>
2023-06-15 21:33:24 -0500

    change styles

--- Views/Estadisticas/NuevaEstadistica.php
+++ Views/Estadisticas/NuevaEstadistica.php
@@ -18,12 +18,12 @@ include_once(BASE_DIR . "../../Views/partials/aside.php");
                         <form action="../../Controllers/EstadisticasController.php?c=3" method="post">
                             <input type="hidden" name="user_id" value="<?php echo $id_usuario; ?>">
 
-                            <div class="col-lg-12 mt-5">
+                            <div class="col-lg-12 mt-5 pt-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header bg-primary">
+                                    <div class="card-header bg-black ">
                                         <h3 class="text-center text-light my-4 fs-4">Crear Nueva Estadistica</h3>
                                     </div>
-                                    <div class="card-body">
+                                    <div class="card-body text-black" style="background-color: #CDA078;">
                                         <div class="row align-items-center mb-3">
                                             <div class="col-md-12 mt-3">
                                                 <div class="form-floating mb-3 mb-md-0 text-center" >
@@ -43,7 +43,19 @@ include_once(BASE_DIR . "../../Views/partials/aside.php");
                                                 <input class="form-control text-center" type="text" name="descripcion" id="descripcion" autocomplete="off">
                                             </div>
                                         </div>
-                                        <button class="btn btn-warning btn-block text-secondary">Agregar A Las Estadisticas Predeterminadas</button>
+
+                                        <div class="mt-4 mb-0">
+                                        <div class="d-grid">
+                                            <button class="btn btn-primary btn-block text-blackx">Agregar Estadisitca</button>
+
+                                        </div>
+                                        <br>
+                                        <div class="d-grid">
+                                            <a class="btn btn-success" href="../Estadisticas/LlenarEstadistica.php">Regresar</a>
+                                        </div>
+                                    </div>
+                                            
+                                        
                                     </div>
                                 </div>
                             </div>
